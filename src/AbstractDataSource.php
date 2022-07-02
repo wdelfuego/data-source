658c23e189b3dbb9a975103d77013166a1a16078
Willem Vervuurt <willem@studiodelfuego.com>
2022-07-02 03:15:36 +0200

    DataSource can now hold a Request and have a default onButtonAction that ireturns the regular GET for that data source

--- src/AbstractDataSource.php
+++ src/AbstractDataSource.php
@@ -5,4 +5,22 @@ namespace Wdelfuego\DataSource;
 abstract class AbstractDataSource
 {
     abstract public function data() : array;
+    
+    private $request = null;
+    
+    public function request()
+    {
+        return $this->request;
+    }
+    
+    public function withRequest($request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+    
+    public function onButtonAction(string $buttonAction, string $buttonId = null)
+    {
+        return $this->data();
+    }
 }
